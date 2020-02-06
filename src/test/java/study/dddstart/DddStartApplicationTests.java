0dd8affa20e7818084803ce6b18cd9f491a21452
Junha Kim <kimjh4930@gmail.com>
2020-02-06 10:07:10 +0900

    init

---
+++ src/test/java/study/dddstart/DddStartApplicationTests.java
@@ -0,0 +1,13 @@
+package study.dddstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
